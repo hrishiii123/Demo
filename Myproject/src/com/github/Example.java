7c22856b88968cb4ab5166b8230afbe3aa9e6249
hrishiii123 <rushikeshbhujbal5262@gmail.com>
2022-12-19 22:20:39 +0530

    Arraylist added

---
+++ Myproject/src/com/github/Example.java
@@ -0,0 +1,14 @@
+package com.github;
+
+import java.util.ArrayList;
+
+public class Example {
+	public static void main(String[] args) {
+		ArrayList al=new ArrayList();
+		al.add(10);
+		al.add(20);
+		al.add(30);
+		System.out.println(al);
+	}
+
+}
